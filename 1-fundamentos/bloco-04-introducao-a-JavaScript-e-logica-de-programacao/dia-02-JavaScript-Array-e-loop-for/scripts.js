476c5eb7569e6b0747431844d89ff74613ab716c
Artur Lemes Moretti <arturl.moretti@gmail.com>
2021-07-29 15:54:59 -0300

    Adiciona exemplo da seçao For/of

--- 1-fundamentos/bloco-04-introducao-a-JavaScript-e-logica-de-programacao/dia-02-JavaScript-Array-e-loop-for/scripts.js
+++ 1-fundamentos/bloco-04-introducao-a-JavaScript-e-logica-de-programacao/dia-02-JavaScript-Array-e-loop-for/scripts.js
@@ -1,5 +1,7 @@
-let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
 
-for (var index = 0; index<groceryList.length; index+=1){
-    console.log(groceryList[index]);
+
+
+for (let nomes of names){
+    console.log(nomes);
 }
