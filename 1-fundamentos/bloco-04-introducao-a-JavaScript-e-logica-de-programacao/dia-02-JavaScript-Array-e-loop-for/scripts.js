8e727433bcd5f9fbb282860caffacceab75bcfe9
Artur Lemes Moretti <arturl.moretti@gmail.com>
2021-07-29 18:13:59 -0300

    Finaliza Ex. 1

--- 1-fundamentos/bloco-04-introducao-a-JavaScript-e-logica-de-programacao/dia-02-JavaScript-Array-e-loop-for/scripts.js
+++ 1-fundamentos/bloco-04-introducao-a-JavaScript-e-logica-de-programacao/dia-02-JavaScript-Array-e-loop-for/scripts.js
@@ -1,7 +1,12 @@
-let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
+// Ex. 1 
 
-
-for (let nomes of names){
-    console.log(nomes);
+for (index = 0; index<numbers.length; index+=1){
+    console.log('Usando o for '+ numbers[index]);
 }
+
+for (let numeros of numbers){
+    console.log('usando o for/of: ' +numeros);
+}
+
