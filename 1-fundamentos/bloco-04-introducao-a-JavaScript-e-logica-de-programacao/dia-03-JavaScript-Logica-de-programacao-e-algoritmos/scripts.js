a669c81b5f7f4364cbe12ee00b58552d399f62de
Artur Lemes Moretti <arturl.moretti@gmail.com>
2021-07-30 18:12:15 -0300

    Adiciona exercicio-2 e 3

--- 1-fundamentos/bloco-04-introducao-a-JavaScript-e-logica-de-programacao/dia-03-JavaScript-Logica-de-programacao-e-algoritmos/scripts.js
+++ 1-fundamentos/bloco-04-introducao-a-JavaScript-e-logica-de-programacao/dia-03-JavaScript-Logica-de-programacao-e-algoritmos/scripts.js
@@ -6,4 +6,29 @@ let ast = [""];
 for (let i = 0 ; i<n;i+=1){
     ast = ast + "*";
 }
-console.log(ast);
+for (let i = 0 ; i<n;i+=1){
+    console.log(ast);
+} 
+
+// Exercício 2
+
+
+let n2 =5;
+let ast2 = [""];
+
+for (let i = 0 ; i<n2;i+=1){
+    ast2 = ast2 + "*";
+    console.log(ast2)
+}
+
+
+// Exercício 3
+
+let n3 =5;
+let ast3 = [""];
+let espacos = [""];
+
+for (let i = 0 ; i<n3;i+=1){
+    ast3 = " " + ast3 + "*";
+    console.log(ast3)
+}
