bf1dc2cf9612b3b7922b6da6976ff545ae619823
Artur Lemes Moretti <arturl.moretti@gmail.com>
2021-08-02 15:47:48 -0300

    Adiciona exercicio 3 e 4 do Conteudo Objetos

--- 1-fundamentos/bloco-04-introducao-a-JavaScript-e-logica-de-programacao/dia-04-objetos-e-funções/scripts.js
+++ 1-fundamentos/bloco-04-introducao-a-JavaScript-e-logica-de-programacao/dia-04-objetos-e-funções/scripts.js
@@ -1,3 +1,4 @@
+console.log("Exercício 1 e 2");
 player = {
     name: "Marta",
     lasName: 'Silva',
@@ -9,3 +10,10 @@ player = {
 }
 
 console.log ('A jogadora ' + player.name + ' tem ' + player.age +' anos de idade')
+
+
+console.log("Exercício 3 e 4");
+
+player["bestInTheWorld"] = [2006, 2007, 2008, 2009, 2010, 2018];
+
+console.log ("A jogadora " + player.name + " foi eleita a melhor do mundo por " + player.bestInTheWorld.length +" vezes.");
