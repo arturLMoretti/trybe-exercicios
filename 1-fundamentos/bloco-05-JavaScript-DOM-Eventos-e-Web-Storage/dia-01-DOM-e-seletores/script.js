cd23a6e71da20f83d2dbec82d49becff6f6dd1c8
Artur Lemes Moretti <arturl.moretti@gmail.com>
2021-08-12 14:56:52 -0300

    Substitui por querySelector ou querySelectorAll

---
+++ 1-fundamentos/bloco-05-JavaScript-DOM-Eventos-e-Web-Storage/dia-01-DOM-e-seletores/script.js
@@ -0,0 +1,7 @@
+let margensDivs = document.getElementsByTagName('div');
+
+for (let i = 0; i < margensDivs.length; i +=1){
+    margensDivs[i].style.borderStyle = 'solid';
+    margensDivs[i].style.borderColor = 'black';
+    margensDivs[i].style.borderWidth = 'thick';
+}
