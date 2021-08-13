809413f0ef8da6a167ce321b6a6076857c43f555
Artur Lemes Moretti <arturl.moretti@gmail.com>
2021-08-13 13:50:16 -0300

    Altera layout Administrador do Tempo daTrybe

--- 1-fundamentos/bloco-05-JavaScript-DOM-Eventos-e-Web-Storage/dia-01-DOM-e-seletores/script.js
+++ 1-fundamentos/bloco-05-JavaScript-DOM-Eventos-e-Web-Storage/dia-01-DOM-e-seletores/script.js
@@ -1,7 +1,36 @@
-let margensDivs = document.getElementsByTagName('div');
+let backgroundUrgente = document.getElementsByClassName('urgent-tasks');
+let backgroundNotUrgente = document.getElementsByClassName('not-urgent-tasks');
+let fundo = document.getElementById('container');
+let rodape = document.getElementById('footer-container');
+let cabecalho = document.getElementById('header-container')
+let urgentes = document.getElementsByClassName('emergency-tasks');
+let naoUrgentes = document.getElementsByClassName('no-emergency-tasks');
 
-for (let i = 0; i < margensDivs.length; i +=1){
-    margensDivs[i].style.borderStyle = 'solid';
-    margensDivs[i].style.borderColor = 'black';
-    margensDivs[i].style.borderWidth = 'thick';
+
+// Alterna background Título dos post-its
+for (let i = 0; i < backgroundUrgente.length; i +=1){
+    backgroundUrgente[i].style.backgroundColor = 'black'
+}
+for (let i = 0; i < backgroundNotUrgente.length; i +=1){
+    backgroundNotUrgente[i].style.backgroundColor = 'blue'
+}
+
+// Altera cor do fundo 
+fundo.style.backgroundColor = 'RGB(146, 168, 209)'
+
+// Altera rodapé
+rodape.style.backgroundColor = 'RGB(0, 161, 112)'
+
+// Altera cabeçalho
+cabecalho.style.backgroundColor = 'rgb(0, 161, 112)';
+
+
+// Alterra cor de fundo Urgentes
+for (let i = 0; i < urgentes.length; i +=1){
+    urgentes[i].style.backgroundColor = 'RGB(247, 202, 201)';
+}
+
+// Altera cor de fundo não-urgentes
+for (let i = 0; i < naoUrgentes.length; i +=1){
+    naoUrgentes[i].style.backgroundColor = 'RGB(245, 223, 77)';
 }
