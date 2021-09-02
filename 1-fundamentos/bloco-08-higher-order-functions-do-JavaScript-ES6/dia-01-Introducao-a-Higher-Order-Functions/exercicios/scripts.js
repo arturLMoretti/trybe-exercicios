773793b619153a5dd3f175fbbc3b2c411e4d5e83
Artur Lemes Moretti <arturl.moretti@gmail.com>
2021-09-02 18:26:21 -0300

    Inicia Bonus

--- 1-fundamentos/bloco-08-higher-order-functions-do-JavaScript-ES6/dia-01-Introducao-a-Higher-Order-Functions/exercicios/scripts.js
+++ 1-fundamentos/bloco-08-higher-order-functions-do-JavaScript-ES6/dia-01-Introducao-a-Higher-Order-Functions/exercicios/scripts.js
@@ -11,15 +11,16 @@ const idCreator = name => {
     return { name: name, email: `${emailId}@trybe.com`}
 };
 
-console.log(idCreator('Artur Lemes Moretti'));
-const newEmployees = () => {
+const newEmployees = (callback) => {
     const employees = {
-      id1: '', // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-      id2: '', // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-      id3: '', // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+      id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+      id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+      id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
     }
     return employees;
   };
+
+  console.log(newEmployees(idCreator));
 
 const checkFunction = (a,b) => a===b ? 'Parabéns você ganhou' : `Resultado: ${b}! Tente novamente`;
 
