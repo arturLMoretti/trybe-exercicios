e41ef18e303d63876fdffd43f6c9ad2b73e04c1e
Artur Lemes Moretti <arturl.moretti@gmail.com>
2021-09-15 19:35:11 -0300

    Finaliza exercicios

---
+++ 1-fundamentos/bloco-09-JavaScript-Assincrono-e-Promises/dia-01-JavaScript-Assincrono-e-Callbacks/exercicios/ex2.js
@@ -0,0 +1,36 @@
+const planetDistanceFromSun = ({ name, distanceFromSun: { value, measurementUnit } }) =>
+  `${name} is ${value} ${measurementUnit} apart from the Sun`;
+
+const mars = {
+  name: "Mars",
+  distanceFromSun: {
+    value: 227900000,
+    measurementUnit: "kilometers",
+  },
+};
+
+const venus = {
+  name: "Venus",
+  distanceFromSun: {
+    value: 108200000,
+    measurementUnit: "kilometers",
+  },
+};
+
+const jupiter = {
+  name: "Jupiter",
+  distanceFromSun: {
+    value: 778500000,
+    measurementUnit: "kilometers",
+  },
+};
+
+console.log(`É esperada a ordem
+Mars
+Jupiter
+Venus
+Enquanto a chamada de Mars é síncrona, as seguintes são assíncrontas,
+com a chamada de Jupiter sendo menor que a de Venus`);
+console.log(planetDistanceFromSun(mars)); // A
+setTimeout(() => console.log(planetDistanceFromSun(venus)), 3000); // B
+setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // C
