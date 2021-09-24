67e35717925a37592b770fded38168760ae5535f
Artur Lemes Moretti <arturl.moretti@gmail.com>
2021-09-24 08:54:52 -0300

    Finaliza exercicio 1

--- 1-fundamentos/bloco-10-Testes-automatizados-com-Jest/dia-01-Primeiros-Passos-no-Jest/exercicios/tests/sum.test.js
+++ 1-fundamentos/bloco-10-Testes-automatizados-com-Jest/dia-01-Primeiros-Passos-no-Jest/exercicios/tests/sum.test.js
@@ -1,5 +1,17 @@
-import sum from '../src/sum.js';
+const sum = require('../src/sum')
 
 test('1 - A soma de 4 e 5 deve ser 9', () => {
     expect(sum(4, 5)).toBe(9)
+});
+
+test('2 - A soma de 0 e 0 deve ser 0', () => {
+    expect(sum(0, 0)).toBe(0)
+});
+
+test('3 - a soma de 4 e "5" deve retornar um erro', () => {
+    expect(() => {sum(4, '5')}).toThrow();
+});
+
+test('4 - a mensagem de erro deve ser "parameters must be numbers"', () => {
+    expect(() => {sum(4, '5')}).toThrowError('parameters must be numbers');
 });
