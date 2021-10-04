78cd3cc168bbfd318d690a85403db25af31d22d2
Artur Lemes Moretti <arturl.moretti@gmail.com>
2021-10-04 19:12:57 -0300

    cria relogio

--- 2-desenvolvimento-frontend/dia-01-introducao-ao-react/testando-meu-computador/src/App.js
+++ 2-desenvolvimento-frontend/dia-01-introducao-ao-react/testando-meu-computador/src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
 import React from 'react';
 
-function App() {
-  const textJSX = 'Hello, JSX';
-  return (<h1>{textJSX}</h1>);
+
+class Text extends React.Component {
+  render(){
+    const textJSX = 'Hello, JSX';
+    return <h1>{textJSX}</h1>;
+  }
 }
 
+function merdaDeRelogio() {
+  return new Date().toLocaleTimeString();
+}
+
+function Horas () {
+  
+  const hora = merdaDeRelogio();
+  console.log(hora);
+  return `Agoras são ${hora}`;
+}
+
+function App() {
+  
+  return (<>
+  <Text></Text>
+  <Horas></Horas>
+    </>); 
+}
 export default App;
