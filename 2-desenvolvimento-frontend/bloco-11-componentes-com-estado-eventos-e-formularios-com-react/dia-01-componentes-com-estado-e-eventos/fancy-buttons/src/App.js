11fc5c43dca0ddef06505db04e30f2df371d44f7
Artur Lemes Moretti <arturl.moretti@gmail.com>
2021-10-20 23:03:37 -0300

    adiciona Para Fixar 2

--- 2-desenvolvimento-frontend/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ 2-desenvolvimento-frontend/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -2,23 +2,15 @@ import './App.css'
 import React from "react";
 import Button from "./components/Button"
 
-const botao1 = () => {
-  console.log('Este é o botão 1');
-}
-const botao2 = () => {
-  console.log('Este é o botão 2');
-}
-const botao3 = () => {
-  console.log('Este é o botão 3');
-}
-
 class App extends React.Component {
+
+
   render() {
     return(
     <div className="flex">
-      <Button botao={botao1} text="Botão 1"/>
-      <Button botao={botao2} text="Botão 2"/>
-      <Button botao={botao3} text="Botão 3"/>
+      <Button text="Botão 1"/>
+      <Button text="Botão 2"/>
+      <Button text="Botão 3"/>
     </div>)
   }
 }
--- 2-desenvolvimento-frontend/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/fancy-buttons/src/components/Button.jsx
+++ 2-desenvolvimento-frontend/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-01-componentes-com-estado-e-eventos/fancy-buttons/src/components/Button.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 
 class Button extends React.Component {
+    constructor() {
+        super();
+        this.botao = this.botao.bind(this);
+    }
+    botao({target}) {
+        console.log(target.innerText ===
+            'Botão 1'?
+            'Clicou Botão 1' : ( target.innerText === 'Botão 2' ?
+            'Clicou Botão 2' : 'Clicou botão 3'));
+    }
     render() {
-        const {text, botao} = this.props;
-        return (<div onClick={botao} className="botao" >
+        const {text} = this.props;
+        return (<div onClick={this.botao} className="botao" >
             {text}
         </div>)
     }
