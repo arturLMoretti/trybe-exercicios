6586714f5944331ab8f4807c91bb03f28797ceb5
Artur Lemes Moretti <arturl.moretti@gmail.com>
2021-11-01 08:14:55 -0300

    Adiciona Para Fixar 3 e 4

--- 2-desenvolvimento-frontend/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-02-formularios-no-React/formulario/src/components/Forms.jsx
+++ 2-desenvolvimento-frontend/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-02-formularios-no-React/formulario/src/components/Forms.jsx
@@ -3,50 +3,46 @@ import React, { Component } from 'react'
 class Form extends Component {
   constructor() {
     super();
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleChange2 = this.handleChange2.bind(this);
-
+    this.genericHandleChange = this.genericHandleChange.bind(this);
     this.state = {
-      valor:'',
+      escolhaValor:'',
       estadoFavorito: '',
+      idade:'',
+      vaiComparecer:''
     };
   }
 
-
-  handleChange(event) {
+  genericHandleChange({target}) {
+    const name = target.name;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
     this.setState({
-      estadoFavorito: event.target.value,
-    });
-  }
-
-  handleChange2(event) {
-    this.setState({
-      valor: event.target.value,
-    });
+      [name]:value,
+    })
   }
 
   render() {
     return (
       <div>
         <h1>Estados e React - Tecnologia fantástica ou reagindo a regionalismos?</h1>
-        <select onClick={this.handleChange2} name="escolhaValor">
+        <select onClick={this.genericHandleChange} name="escolhaValor">
           <option value="valor1">Valor 1</option>
           <option value="valor2">Valor 2</option>
           <option value="valor3">Valor 3</option>
         </select>
         <form className="form">
           <label>
-            Diga qual o seu Estado favorito! De React ou do Brasil, você decide! =)
-              <textarea name="estadoFavorito" value={this.state.estadoFavorito} onChange={this.handleChange} />
+            {'Diga qual o seu Estado favorito! De React ou do Brasil, você decide! =)'}
+              <textarea name="estadoFavorito" value={this.state.estadoFavorito} onChange={this.genericHandleChange} />
           </label>
           <input
             type="number"
             name="idade"
+            onChange={this.genericHandleChange}
           />
           <input
             type="checkbox"
             name="vaiComparecer"
+            onChange={this.genericHandleChange}
           />
         </form>
       </div>
